6533c14e74b67511a2605aa5369b9fb1bd369219
bshanmugaraj <53233353+bshanmugaraj@users.noreply.github.com>
2023-12-14 16:43:35 +0530

    Update Jenkinsfile-bkp.groovy

--- Jenkinsfile-bkp.groovy
+++ Jenkinsfile-bkp.groovy
@@ -89,7 +89,7 @@ fi
             sh "git config user.name bshanmugaraj"
             sh "git add ."
             sh "git commit -m 'Commit triggered Build: ${env.BUILD_NUMBER}'"
-            sh "git push https://${Username}:${encodedPassword}@github.com/${Username}/bshanmugaraj/test3.git"     
+            sh "git push https://${Username}:${encodedPassword}@github.com/${Username}/bshanmugaraj/test3.git HEAD:master"     
             }
         }
      }
