b9bb70efc3000fad20b0f46effe3767d44a615f3
bshanmugaraj <53233353+bshanmugaraj@users.noreply.github.com>
2023-12-14 18:34:13 +0530

    Update Jenkinsfile-bkp.groovy

--- Jenkinsfile-bkp.groovy
+++ Jenkinsfile-bkp.groovy
@@ -83,7 +83,7 @@ fi
       }
         stage('Update GIT') {
             catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
-            withCredentials([sshUserPrivateKey(credentialsId: 'eeadb089-6f96-412c-96f8-0e7b2f348d49', keyFileVariable: 'ec2-user')]) {
+            withCredentials([sshUserPrivateKey(credentialsId: 'eeadb089-6f96-412c-96f8-0e7b2f348d49', keyFileVariable: 'jenkins')]) {
             sh "git config user.email shanmugarajb.97@gmail.com"
             sh "git config user.name bshanmugaraj"
             sh "git add ."
